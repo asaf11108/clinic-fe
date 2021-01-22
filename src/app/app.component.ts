b356f297567c85b7439aa58c48a767f117b08ad9
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-01-22 22:39:24 +0200

    daclared global obj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clinic-fe';
+  
+  // signUp() {
+  //   Auth.signUp({
+  //     username: 'irisreg7',
+  //     password: '62626262',
+  //     attributes: {
+  //       email: 'irisreg7@gmail.com'
+  //     }
+  //   }).then(({ user }) => {
+  //     console.log(user)
+  //   }).catch(error => {
+  //     console.log('error signing up:', error);
+  //   })
+  // }
+  ngOnInit(): void {
+  //   setTimeout(() => {
+  //     this.signUp();
+
+  //   }, 5000)
+  }
+
 }
