778be811817c5eb6f949209d57deb2d09b521226
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-02-23 14:34:06 +0200

    info style

--- src/app/components/patient-file-info/patient-file-info.component.ts
+++ src/app/components/patient-file-info/patient-file-info.component.ts
@@ -1,11 +1,12 @@
 import { Patient } from './../../model/patient';
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-patient-file-info',
   templateUrl: './patient-file-info.component.html',
   styleUrls: ['./patient-file-info.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class PatientFileInfoComponent implements OnInit {
   @Input() patient: Patient;
