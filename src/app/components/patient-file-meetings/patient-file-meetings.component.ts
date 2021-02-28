c43c562a896b944a4efdbede071a1ce5b883c539
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-02-28 10:29:48 +0200

    added meetings

--- src/app/components/patient-file-meetings/patient-file-meetings.component.ts
+++ src/app/components/patient-file-meetings/patient-file-meetings.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Meeting } from 'src/app/model/meeting';
 
 @Component({
   selector: 'app-patient-file-meetings',
   templateUrl: './patient-file-meetings.component.html',
   styleUrls: ['./patient-file-meetings.component.sass'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class PatientFileMeetingsComponent implements OnInit {
+  dataSource = new MatTableDataSource<Meeting>();
+  columns = ['date', 'description', 'weight', 'BMI'];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource.data = [{
+      date: '03/08/20',
+      description: 'aaaaaad dawdawddd wdawda',
+      weight: 77,
+      BMI: 3
+    }];
   }
 
 }
---
+++ src/app/model/meeting.ts
@@ -0,0 +1,6 @@
+export interface Meeting {
+    date: string;
+    description: string;
+    weight: number;
+    BMI: number;
+  }
