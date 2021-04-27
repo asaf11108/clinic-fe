c45873134243e2a53004e5fb506c0802f9977c4f
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-04-27 15:59:33 +0300

    organize client auth

--- src/app/services/amplify-auth.service.ts
+++ src/app/services/amplify-auth.service.ts
@@ -1,19 +1,34 @@
+import { APIService } from './../API.service';
 import { Injectable } from '@angular/core';
+import { Auth } from 'aws-amplify';
+import awsconfig from 'src/aws-exports';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import gql from 'graphql-tag';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AmplifyAuthService {
+  readonly client;
 
-  constructor() { }
-
-  // readonly client = new AWSAppSyncClient({
-  //   url: awsconfig.aws_appsync_graphqlEndpoint,
-  //   region: awsconfig.aws_appsync_region,
-  //   auth: {
-  //     type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
-  //     jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
-  //   },
-  //   offlineConfig: {}
-  // });
+  constructor(private apiService: APIService) {
+    this.client = new AWSAppSyncClient({
+      url: awsconfig.aws_appsync_graphqlEndpoint,
+      region: awsconfig.aws_appsync_region,
+      auth: {
+        type: awsconfig.aws_appsync_authenticationType as any,
+        jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
+      },
+      disableOffline: true
+    })
+    this.client.query({
+      query: gql(apiService.ListPatients().query),
+    })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(res => {
+        console.log(res)
+      })
+  }
 }
